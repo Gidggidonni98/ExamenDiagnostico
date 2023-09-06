c29be990d0546c74e31e73084f5afdf72974a172
Kemish Jimenez <20193tn178@utez.edu.mx>
2023-09-05 23:40:18 -0600

    Terminado

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const findUser = async() =>{
-    await $.ajax({
-        method: "GET",
-        headers: { "Accept": "application/json" },
-        url: 'https://reqres.in/api/users?page=2'
-    }).done(function(res){
-        content = "";
-        datos = res.data;
-        console.log(datos);
-        for (let i = 0; i < datos.length; i++) {
-            content += `
+//GET////////////////////////////////
+
+const findUser = async () => {
+  await $.ajax({
+    method: "GET",
+    headers: { Accept: "application/json" },
+    url: "https://reqres.in/api/users",
+  }).done(function (res) {
+    content = "";
+    datos = res.data;
+    for (let i = 0; i < datos.length; i++) {
+      content += `
             <tr class="text-center">
                 <td>${datos[i].id}</td>
                 <td>
@@ -18,69 +19,135 @@ const findUser = async() =>{
                 <td>${datos[i].last_name}</td>
                 <td>${datos[i].email}</td>
                 <td>
-                    <button class='btn btn-primary' data-toggle='modal' onclick='getInfoUser()' data-target='#detallesCliente'><i class='fas fa-info-circle'></i></button>
+                    <button class='btn btn-primary' data-toggle='modal' onclick='getInfoUser(${datos[i].id})' data-target='#update'><i class='fas fa-info-circle'></i></button>
                 </td>
                 <td>
-                    <button class='btn btn-danger' data-toggle='modal' onclick='getIdUser()' data-target='#eliminar'><i class="fas fa-trash"></i></button>
+                    <button class='btn btn-danger' data-toggle='modal'  data-target='#eliminar'><i class="fas fa-trash"></i></button>
                 </td>
             </tr>
                 `;
-        };
-        $("#tablaUsers > tbody").html(content)
-    })
-}
+    }
+    $("#tablaUsers > tbody").html(content);
+  });
+};
 
 findUser();
 
+//CREATE////////////////////////////
 
-const createUser = async() => {
-    let first_name = document.getElementById('first_name').value;
-    let last_name = document.getElementById('last_name').value;
-    let email = document.getElementById('email').value;
-    if (first_name != "" || email != "" || last_name) {
-        await $.ajax({
-            type: 'POST',
-            headers: { "Accept": "application/json" },
-            url: "https://reqres.in/api/users?page=2",
-            data: { first_name, last_name, email }
-        }).done(res => {
-            if (res.status === 200) {
-                Swal.fire({
-                    title: "Hubo un error al registrar",
-                    confirmButtonText: "Aceptar",
-                    icon: "error",
-                });
-                findCliente();
-            } else {
-                Swal.fire({
-                    title: "Se ha creado correctamente",
-                    confirmButtonText: "Aceptar",
-                    icon: "success",
-                });
-                findCliente();
-            }
-        });
-    } else {
-        Swal.fire({
-            title: "Rellena los campos primero",
-            confirmButtonText: "Aceptar",
-            icon: "error",
-        })
+const createUser = async () => {
+  let firstName = document.getElementById("first_name").value;
+  let lastName = document.getElementById("last_name").value;
+  let correo = document.getElementById("email").value;
+  if (firstName != "" || correo != "" || lastName) {
+    let datos = {
+      first_name: firstName,
+      last_name: lastName, 
+      email: correo
     }
-};
+    
+    fetch('https://reqres.in/api/users', {
+      method: "POST",
+      body: JSON.stringify(datos),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => {
+      console.log(response)
+      document.getElementById("first_name").value = "";
+      document.getElementById("last_name").value = "";
+      document.getElementById("email").value = "";
+      $('#createUser').modal('hide')
+      Swal.fire({
+        title: "Se ha creado correctamente",
+        confirmButtonText: "Aceptar",
+        icon: "success",
+      });
+    }) 
+    .catch(err => console.log(err));
 
 
-const getIdUser = async id => {
-    document.getElementById("id_deleteUser").value = id;
-};
-
-
-const downUser = async() => {
-    let id = document.getElementById('id_deleteUser').value;
-    await $.ajax({
-        type: 'DELETE',
-        url:'https://reqres.in/api/users?page=2' + id
-    }).done(res => {
-        console.log(res);
+  } else {
+    Swal.fire({
+      title: "Rellena los campos primero",
+      confirmButtonText: "Aceptar",
+      icon: "error",
     });
+  }
 };
+
+//DELETE////////////////////////////////
+
+const downUser = async () => {
+  fetch("https://reqres.in/api/users/2", {
+    method: "DELETE",
+  })
+    .then((res) => {
+      console.log(res);
+      Swal.fire({
+        title: "Se ha eliminado correctamente",
+        confirmButtonText: "Aceptar",
+        icon: "success",
+      });
+      $('#eliminar').modal('hide')
+    })
+    
+};
+
+
+//UPDATE////////////////////////////////
+getInfoUser = (id) => {
+  fetch('https://reqres.in/api/users/'+id)
+  .then(response => response.json()) 
+  .then(data => {
+    datos = data.data;
+    console.log(datos.first_name)
+    document.getElementById("id_update").value = datos.id;
+    document.getElementById("first_name_update").value = datos.first_name;
+    document.getElementById("last_name_update").value = datos.last_name;
+    document.getElementById("email_update").value = datos.email;
+  })
+  
+}
+
+updateUser = () => {
+  id_update = document.getElementById("id_update").value;
+  first_name_update = document.getElementById("first_name_update").value;
+  last_name_update = document.getElementById("last_name_update").value;
+  email_udpate = document.getElementById("email_update").value;
+
+  if (first_name_update != "" || email_udpate != "" || last_name_update) {
+    let datos = {
+      first_name: first_name_update,
+      last_name: last_name_update, 
+      email: email_udpate,
+    }
+    
+    fetch('https://reqres.in/api/users/' + id_update, {
+      method: "PUT",
+      body: JSON.stringify(datos),
+      headers: {"Content-type": "application/json; charset=UTF-8" ,  Accept: "application/json" }
+    })
+    .then(response => {
+      console.log(response)
+      $('#update').modal('hide')
+      Swal.fire({
+        title: "Se ha actualizado correctamente",
+        confirmButtonText: "Aceptar",
+        icon: "success",
+      });
+    }) 
+    .catch(err => console.log(err));
+
+
+  } else {
+    Swal.fire({
+      title: "Rellena los campos primero",
+      confirmButtonText: "Aceptar",
+      icon: "error",
+    });
+  }
+
+}
+
+
+
